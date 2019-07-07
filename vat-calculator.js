af675c549632d69d9994793632409018bbf0aac4
Khari Woods <khariwoods3@gmail.com>
2019-07-07 15:41:46 -0400

    First Commit

---
+++ vat-calculator.js
@@ -0,0 +1,76 @@
+const netPrice = document.querySelector('#net-price')
+let vatRate = document.querySelector('#vat-rate')
+vatRate.value = '12'
+const vatForm = document.querySelector('#vat-form')
+const excludeVat = document.querySelector('#exclude-vat')
+const includeVat = document.querySelector('#include-vat')
+let calculationResults = document.querySelector('#results')
+
+const errorMessage = '<h2> Error! You Must choose to include or Exclude VAT</h2>'
+
+const disclaimer = '<h6>Note: This application calculates the amount of Value Added Tax added or removed from the net price. We are not responsible for any loss that you may encounter using</h6>'
+
+console.log(includeVat)
+let vatPrice
+let vatAmount
+
+
+// Display Resuts in a div
+const displayResults = function(vatPrice,vatRate,operation){
+	return '<h3>Total Cost: $' + vatPrice.toFixed(2) +  '</h3>'+'<h4> VAT Amount: $ ' + vatAmount.toFixed(2) + '</h4>' + '<h4> VAT Rate: '+ vatRate + ' % </h4>' + '<h4> Operation: ' + operation+'</h4>' + disclaimer
+}
+// Remove Vat From an Net Price
+const removeVat = function(netPrice,vatRate){
+	let operation = 'Exclude VAT'
+	vatPrice = (netPrice - ((vatRate/100) * netPrice))
+	vatAmount = (vatRate/100) * netPrice
+	calculationResults.innerHTML = displayResults(vatPrice,vatRate,operation)
+}
+
+// Add Vat to Net Price
+const addVat = function(netPrice,vatRate){
+	let operation = 'Include VAT'
+	vatPrice = (netPrice * ((vatRate/100)+1))
+	vatAmount = (vatRate/100) * netPrice
+	calculationResults.innerHTML = displayResults(vatPrice,vatRate,operation)
+}
+
+
+const validateForm = function(formValue1,formValue2){
+	if(formValue1 === '' || formValue2 ===''){
+		alert('Please complete both forms! ')
+	}
+}
+
+
+	
+
+
+vatForm.addEventListener('submit',function(e){
+	e.preventDefault(e)
+	validateForm(netPrice.value,vatRate.value)
+	let parsedPrice = JSON.parse(netPrice.value)
+	let parsedRate = JSON.parse(vatRate.value)
+
+if(excludeVat.checked){
+	removeVat(parsedPrice,parsedRate)
+	$('#results').fadeIn(4300)
+}else if(includeVat.checked){
+	addVat(parsedPrice,parsedRate)
+	$('#results').fadeIn(4300)
+}else{
+	calculationResults.innerHTML = errorMessage
+}
+
+})
+
+
+
+
+
+
+	
+
+
+
+
